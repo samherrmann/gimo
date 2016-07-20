e0edc2e41e339837cedcb5b495e6ae79c60dd581
Sam Herrmann <webocular@gmail.com>
2016-07-20 01:38:58 -0400

    Remove unused makeSlice function

--- utils.go
+++ utils.go
@@ -1,20 +1,6 @@
 package gimo
 
-import (
-	"reflect"
-
-	"gopkg.in/mgo.v2"
-)
-
-// makeSlice returns a slice of i as an interface{}.
-// Noteworthy resource: http://stackoverflow.com/a/25386460/3634032
-func makeSlice(i interface{}) interface{} {
-	iType := reflect.TypeOf(i)
-	sliceType := reflect.SliceOf(iType)
-	goValue := reflect.New(sliceType)
-	goValue.Elem().Set(reflect.MakeSlice(sliceType, 0, 0))
-	return goValue.Interface()
-}
+import "gopkg.in/mgo.v2"
 
 // dialDB establishes a new session to the mongoDB cluster
 // identified by info.
